30414430034e0a34f9ed295443e10574c91aae4f
Igor Bereza <igor.bereza@gmail.com>
2021-03-01 13:19:40 +0200

    used setdefault

--- Bereza_Igor_dz_3/task_3_4.py
+++ Bereza_Igor_dz_3/task_3_4.py
@@ -1,17 +1,11 @@
 def thesaurus_adv(*arg):
     _dict = {}
-    for item in arg:
-        name, surname = item.split(" ")
-        if surname[0] in _dict:
-            if name[0] in _dict[surname[0]]:
-                _dict[surname[0]][name[0]].append(item)
-            else:
-                _dict[surname[0]][name[0]] = [item]
-        else:
-            _dict[surname[0]] = {}
-            _dict[surname[0]][name[0]] = [item]
+    for i in arg:
+        _dict.setdefault(i[i.index(' ') + 1], {}).setdefault(i[0], []) \
+            .append(i)
+
     return _dict
 
 
-print(thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев",
-                    "Илья Иванов", "Анна Савельева"))
+print(thesaurus_adv("Иван Сергеев", "Петр Алексеев",
+                    "Илья Иванов", "Анна Савельева", "Инна Серова"))
