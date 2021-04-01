1feed42c346d6ba688dbc4eb6c6f8467d2f8ac85
Igor Bereza <igor.bereza@gmail.com>
2021-04-01 06:44:58 +0300

    dz_10

--- Bereza_Igor_dz_10/task_10_3.py
+++ Bereza_Igor_dz_10/task_10_3.py
@@ -2,22 +2,25 @@ class Cell:
     def __init__(self, amount_nucleus):
         self.amount_nucleus = int(amount_nucleus)
 
+    def __str__(self):
+        return str(self.amount_nucleus)
+
     def __add__(self, other_cell):
-        return self.amount_nucleus + other_cell.amount_nucleus
+        return Cell(self.amount_nucleus + other_cell.amount_nucleus)
 
     def __sub__(self, other_cell):
         result = self.amount_nucleus - other_cell.amount_nucleus
         if result > 0:
-            return result
+            return Cell(result)
         else:
             return ('Разность количества ячеек двух клеток '
                     'не может быть меньше нуля')
 
     def __mul__(self, other_cell):
-        return self.amount_nucleus * other_cell.amount_nucleus
+        return Cell(self.amount_nucleus * other_cell.amount_nucleus)
 
     def __floordiv__(self, other_cell):
-        return self.amount_nucleus // other_cell.amount_nucleus
+        return Cell(self.amount_nucleus // other_cell.amount_nucleus)
 
     def make_order(self, cell, num_row):
         result = ''
@@ -31,8 +34,8 @@ class Cell:
 
 cell_1 = Cell(12)
 cell_2 = Cell(15)
-print(f'Кол-во ячеек первой клетки: {cell_1.amount_nucleus}')
-print(f'Кол-во ячеек второй клетки: {cell_2.amount_nucleus}')
+print(f'Кол-во ячеек первой клетки: {cell_1}')
+print(f'Кол-во ячеек второй клетки: {cell_2}')
 print('-------')
 print(f'Сумма количества ячеек двух клеток: {cell_1 + cell_2}')
 print('-------')
